224a7aa2da1618039b816fab6415fd3113eaa64b
HyeongChang Lee <hc9904@hanyang.ac.kr>
2019-09-29 14:43:06 +0900

    Update ACM

---
+++ ACM/hello.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define fr(i, a, b) for (int i = a; i < b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef priority_queue<int> pqi;
+typedef priority_queue<ll> pql;
+
+const int INF = 2147483647;
+const int MOD = 1000000007;
+const double PI = acos(-1);
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  return 0;
+}
